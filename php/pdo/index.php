b70f34f68076e4977d0f103e7a71885b8f71056d
Tripeur76 <59241607+Tripeur76@users.noreply.github.com>
2020-09-14 01:17:48 +0200

    Update index.php

--- php/pdo/index.php
+++ php/pdo/index.php
@@ -1,1 +1,20 @@
+<?php
+  try{
+        $bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8','root', 'password');
+        $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+  catch(Exception $e){
+        die('Erreur :'.$e->getMessage());
+    }
 
+   $req = $bdd->query('SELECT * FROM users ORDER BY id DESC');
+
+   echo '<ul>';
+   while ($data = $req->fetchAll())
+   {
+       echo '<li>'.$data['fname'].' '.strtoupper($data['.name.']).'</li>';
+   }
+   echo '</ul>';
+
+   $req->closeCursor(); // Fermeture de la connexion
+?>
