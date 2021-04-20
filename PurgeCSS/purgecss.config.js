2bada8d54f31fedd0a99a47ce616038a5d371cdc
steveldev <59241607+steveldev@users.noreply.github.com>
2021-04-20 20:09:44 +0200

    Create purgecss.config.js

---
+++ PurgeCSS/purgecss.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: ['index.html'],
+    css: [
+        './assets/css/bootstrap.min.css',
+        './assets/css/style.css',    
+    ],
+    output: './build/',
+    keyframes: true,
+    variables: true,
+    rejected: true,
+
+  }
